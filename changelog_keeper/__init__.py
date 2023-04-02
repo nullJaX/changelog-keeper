97ffa9a9194059591ebf63a80d922fc847023418
nullJaX <lewanski.jacek@gmail.com>
2023-04-02 02:51:25 +0200

    feat: release 1.0.0

--- changelog_keeper/__init__.py
+++ changelog_keeper/__init__.py
@@ -1,5 +1,5 @@
 """
 changelog keeper (clkpr) - Command line utility for keeping your changelog tidy
 """
-VERSION_PARTS = (0, 9, 0)
+VERSION_PARTS = (1, 0, 0)
 VERSION = ".".join(map(str, VERSION_PARTS))
